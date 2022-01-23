616c62970c897cd2c4c7a4a2eaf6228e1e685c32
Mete Han Özgeldi <metehanozgeldi@gmail.com>
2022-01-23 16:01:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //Server
 const WebSocket = require('ws');
-const server = new WebSocket.Server({ host: '0.0.0.0', port: 5000 }, () => {
+const server = new WebSocket.Server({ host: '3.209.172.72', port: 5000 }, () => {
     console.log(`${new Date().toLocaleString()} - Listening on port 5000...`);
 });
 server.on('connection', (ws, req) => {
